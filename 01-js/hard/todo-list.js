2b582174bb510f6b23bc2d3a8238a1b01d69d65b
prashantr2 <prashantrawat2ofc@gmail.com>
2024-10-18 00:20:33 +0530

    Done 1st hard ass.

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -11,7 +11,47 @@
 */
 
 class Todo {
+  constructor() {
+    this.todos = [];
+  };
 
-}
+  add(todo) {
+    this.todos.push(todo); 
+  }
+  
+  remove(idx) {
+    if (idx >= this.todos.length) return;
+    this.todos.splice(idx, 1);
+  }
+  
+  update(idx, newTodo) {
+    if (idx >= this.todos.length) return;
+    this.todos[idx] = newTodo;
+  }
+  
+  getAll() {
+    return this.todos;
+  }
+  
+  get(idx) {
+    if (idx >= this.todos.length) return null;
+    return this.todos[idx];
+  }
+  
+  clear() {
+    this.todos = [];
+  }
+};
+
+// let a = new Todo();
+// a.add('abc');
+// console.log(a.getAll())
+// a.add('def');
+// a.add('ghi');
+// console.log(a.get(1))
+// a.remove(0);
+// console.log(a.getAll())
+// a.clear();
+// console.log(a.getAll())
 
 module.exports = Todo;
